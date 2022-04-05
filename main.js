d55539fc549fb67104156ab45a175881ff07eada
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-05 07:11:33 +0100

    Finished it up by checking the linters

--- main.js
+++ main.js
@@ -4,20 +4,18 @@ const sidebar = document.querySelector('.sidebar');
 // const righthandOfLogo = document.querySelector('.righthand-of-logo');
 const addToListing = document.querySelector('.ul-listing');
 const navItems = document.querySelectorAll('.navitem-sidebar');
-const removeEnve = document.querySelector('.logo-portfolio-right-content-2')
+const removeEnve = document.querySelector('.logo-portfolio-right-content-2');
 
 menu.addEventListener('click', () => {
   sidebar.classList.toggle('show-sidebar');
   addToListing.classList.add('add-to-listing');
   removeEnve.style.display = 'none';
-
-
 });
 
 closeMenu.addEventListener('click', () => {
   sidebar.classList.toggle('show-sidebar');
-  addToListing.classList.toggle('ul-listing')
-  addToListing.classList.toggle('add-to-listing')
+  addToListing.classList.toggle('ul-listing');
+  addToListing.classList.toggle('add-to-listing');
 });
 
 navItems.forEach((navItem) => {
