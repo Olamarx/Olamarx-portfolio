c83a8f0181eec4b454e6e98de676bc0e4bf28891
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-05 04:39:03 +0100

    Removed the envelop under the toggle

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const closeMenu = document.querySelector('.close-menu');
 const sidebar = document.querySelector('.sidebar');
 // const righthandOfLogo = document.querySelector('.righthand-of-logo');
 const addToListing = document.querySelector('.ul-listing');
+const removeEnve = document.querySelector('.logo-portfolio-right-content-2')
 
 menu.addEventListener('click', () => {
   sidebar.classList.toggle('show-sidebar');
   addToListing.classList.add('add-to-listing');
+  removeEnve.style.display = 'none';
 });
 
 closeMenu.addEventListener('click', () => {
