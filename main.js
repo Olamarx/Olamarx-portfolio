56b6a0e632d1e2897a72459d4f467a51299deab2
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-05 04:17:11 +0100

    bunch of poor code here

--- main.js
+++ main.js
@@ -3,12 +3,33 @@ const closeMenu = document.querySelector('.close-menu');
 const sidebar = document.querySelector('.sidebar');
 // const righthandOfLogo = document.querySelector('.righthand-of-logo');
 const addToListing = document.querySelector('.ul-listing');
+const hideUnion = document.querySelector('.logo-portfolio-right-content-2')
+const toggle = document.querySelector('.sidebar-none')
+const hamburger = document.querySelector('.hamburger')
+// const portfolio = document.getElementById('portfolio')
+const menuItem = document.querySelectorAll('.portfolio')
 
 menu.addEventListener('click', () => {
   sidebar.classList.toggle('show-sidebar');
   addToListing.classList.add('add-to-listing');
+  hideUnion.style.display = 'none'
 });
+
+
+menuItem.addEventListener('click', function() {
+    closeMenu.style.display = 'none'
+    sidebar.style.display = 'none'
+})
+
+// function goToLink() {
+//     if(menuItem.addEventListener('click')){
+//         sidebar.style.display = 'none'
+//     } 
+// }
+
+// portfolio.addEventListener('click', goToLink)
 
 closeMenu.addEventListener('click', () => {
   sidebar.classList.remove('show-sidebar');
 });
+
