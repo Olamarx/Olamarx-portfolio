ffe80b1c90dc1120c71cc84b09b25365107edcdf
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-07 00:35:37 +0100

    Fix the for loop

--- main.js
+++ main.js
@@ -27,54 +27,78 @@ const popupContent = {
   headerMobile: 'Multi Post Stories',
   headerDesktop: 'Keeping track of hundreds  of components website',
   closeIcon: './images/times.svg',
-  descriptionDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-  descriptionMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-  timeFont: './images/times.svg',
+  descriptionDesktop: "",
+  descriptionMobile: "",
+  timeFont: '',
   featuredImage: './images/popupImg.svg',
-  liveVersion: 'https://olamarx.github.io/Olamarx-portfolio/',
-  sourceLink: 'https://github.com/Olamarx/Olamarx-portfolio',
-  githubfont: '<img src="./images/githubfont.svg" alt="">',
-  fontLive: '<img src="./images/livepreview.svg" alt="Live-preview">',
+  liveVersion: '',
+  sourceLink: '',
+  githubfont: '',
+  fontLive: '">',
 
   buttons: {
-    html: 'HTML',
-    bootstrap: 'Bootstrap',
-    ruby: 'Ruby on rails',
+    html: '',
+    bootstrap: '',
+    ruby: '',
   },
 };
 
-const div1Popup = document.createElement('div');
-div1Popup.innerHTML = `<div>
-    <div class="pop-up-header-times">
-    <h3 class="pop-header-h3"> ${popupContent.headerMobile} </h3>
-    <h3 class="pop-header-h3-desktop"> ${popupContent.headerDesktop} </h3>
-    <img data-close-button class="popup-close-barr" src="${popupContent.timeFont}" alt="close-bar">
-  </div>
- 
-  <ul class="html-bootstrap-ruby-on-rails">
-  <li class="list-btn"><a href="#"><button class="the-listed-btns"> ${popupContent.buttons.html} </button> </a></li>
-  <li class="list-btn">    <a href="#"><button class="the-listed-btns"> ${popupContent.buttons.bootstrap} </button> </a> </li>
-  <li class="list-btn">     <a href="#"><button class="the-listed-btns"> ${popupContent.buttons.ruby} </button> </a>   </li>
-  </ul>
-  <div class="popup-container">
-  <div class="popup-image-and-content-container">
-  <div class="popup-image-container">
-  <img src="${popupContent.featuredImage}" alt="Image">
-  </div>
-  <div class="popup-content-description-and-buttons">
-  <h5 class="popup-content-description desktopviewp">
-    ${popupContent.descriptionDesktop}
-  </h5>
 
-  <h5 class="popup-content-description mobileviewp">
-    ${popupContent.descriptionMobile}
-  </h5>
-  <ul class="see-live-and-source-btns">
-  <li><a href=" ${popupContent.liveVersion}"> <button class="link-list-btn"> See Live ${popupContent.fontLive} </button> </a></li>
-  <li><a href=" ${popupContent.sourceLink}">  <button class="link-list-btn"> See Source ${popupContent.githubfont}  </button>  </a></li>
-  </ul> </div>  </div>  </div>  </div>  </div>`;
+const projects = [{
+  name: "Multi Post Stories",
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: "./images/times.svg",
+  technologies: ["HTML", "Bootstrap", "Ruby on rails"],
+  link: "https://github.com/Olamarx/Olamarx-portfolio",
+},
 
-const openBtn = document.querySelectorAll('[data-modal-target]');
+
+{
+  name: "Keeping track of hundreds  of components website",
+  description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+  image: "./images/times.svg",
+  technologies: ["HTML", "Bootstrap", "Ruby on rails"],
+  link: "https://olamarx.github.io/Olamarx-portfolio/",
+}
+]
+
+
+
+for (let i = 0; i<projects.length; i++) {
+  const div1Popup = document.createElement('div');
+  div1Popup.innerHTML = `<div>
+      <div class="pop-up-header-times">
+      <h3 class="pop-header-h3"> ${projects[i].name} </h3>
+      <h3 class="pop-header-h3-desktop"> ${projects[i].description} </h3>
+      <img data-close-button class="popup-close-barr" src="" alt="close-bar">
+    </div>
+   
+    <ul class="html-bootstrap-ruby-on-rails">
+    <li class="list-btn"><a href="#"><button class="the-listed-btns"> ${popupContent.buttons.html} </button> </a></li>
+    <li class="list-btn">    <a href="#"><button class="the-listed-btns"> ${popupContent.buttons.bootstrap} </button> </a> </li>
+    <li class="list-btn">     <a href="#"><button class="the-listed-btns"> ${popupContent.buttons.ruby} </button> </a>   </li>
+    </ul>
+    <div class="popup-container">
+    <div class="popup-image-and-content-container">
+    <div class="popup-image-container">
+    <img src="${popupContent.featuredImage}" alt="Image">
+    </div>
+    <div class="popup-content-description-and-buttons">
+    <h5 class="popup-content-description desktopviewp">
+      ${popupContent.descriptionDesktop}
+    </h5>
+  
+    <h5 class="popup-content-description mobileviewp">
+      ${popupContent.descriptionMobile}
+    </h5>
+    <ul class="see-live-and-source-btns">
+    <li><a href=" ${popupContent.liveVersion}"> <button class="link-list-btn"> See Live <img src="./images/livepreview.svg" alt="Live-preview"> </button> </a></li>
+    <li><a href=" ${popupContent.sourceLink}">  <button class="link-list-btn"> See Source <img src="./images/githubfont.svg" alt="Github">  </button>  </a></li>
+    </ul> </div>  </div>  </div>  </div>  </div>`;
+
+
+
+    const openBtn = document.querySelectorAll('[data-modal-target]');
 const popUpSection = document.querySelector('.pop-up-section');
 let closeBtn;
 
@@ -89,3 +113,6 @@ openBtn.forEach((button) => {
     });
   });
 });
+}
+
+
