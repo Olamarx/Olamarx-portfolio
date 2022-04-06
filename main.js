fbc840464f17e5323f95538e456a4859e902a1db
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-06 21:28:10 +0100

    Clean up the code.

--- main.js
+++ main.js
@@ -24,31 +24,31 @@ navItems.forEach((navItem) => {
 });
 
 const popupContent = {
-  'headerMobile': 'Multi Post Stories',
-  'headerDesktop': 'Keeping track of hundreds  of components website',
-  'closeIcon': './images/times.svg',
-  'descriptionDesktop': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-  'descriptionMobile': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
-    'timeFont': "./images/times.svg",
-  'featuredImage': './images/popupImg.svg',
-  'liveVersion': 'https://olamarx.github.io/Olamarx-portfolio/',
-  'sourceLink': 'https://github.com/Olamarx/Olamarx-portfolio',
-  'githubfont': '<img src="./images/githubfont.svg" alt="">',
-  'fontLive': '<img src="./images/livepreview.svg" alt="Live-preview">',
-  
-  'buttons': {
-    'html': 'HTML',
-    'bootstrap': 'Bootstrap',
-    'ruby': 'Ruby on rails'
-  }
-}
+  headerMobile: 'Multi Post Stories',
+  headerDesktop: 'Keeping track of hundreds  of components website',
+  closeIcon: './images/times.svg',
+  descriptionDesktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
+  descriptionMobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  timeFont: './images/times.svg',
+  featuredImage: './images/popupImg.svg',
+  liveVersion: 'https://olamarx.github.io/Olamarx-portfolio/',
+  sourceLink: 'https://github.com/Olamarx/Olamarx-portfolio',
+  githubfont: '<img src="./images/githubfont.svg" alt="">',
+  fontLive: '<img src="./images/livepreview.svg" alt="Live-preview">',
+
+  buttons: {
+    html: 'HTML',
+    bootstrap: 'Bootstrap',
+    ruby: 'Ruby on rails',
+  },
+};
 
 const div1Popup = document.createElement('div');
-div1Popup.innerHTML =`<div>
+div1Popup.innerHTML = `<div>
     <div class="pop-up-header-times">
-    <h3 class="pop-header-h3"> ${popupContent['headerMobile']} </h3>
-    <h3 class="pop-header-h3-desktop"> ${popupContent['headerDesktop']} </h3>
-    <img data-close-button class="popup-close-barr" src="${popupContent['timeFont']}" alt="close-bar">
+    <h3 class="pop-header-h3"> ${popupContent.headerMobile} </h3>
+    <h3 class="pop-header-h3-desktop"> ${popupContent.headerDesktop} </h3>
+    <img data-close-button class="popup-close-barr" src="${popupContent.timeFont}" alt="close-bar">
   </div>
  
   <ul class="html-bootstrap-ruby-on-rails">
@@ -72,14 +72,13 @@ div1Popup.innerHTML =`<div>
   <ul class="see-live-and-source-btns">
   <li><a href=" ${popupContent.liveVersion}"> <button class="link-list-btn"> See Live ${popupContent.fontLive} </button> </a></li>
   <li><a href=" ${popupContent.sourceLink}">  <button class="link-list-btn"> See Source ${popupContent.githubfont}  </button>  </a></li>
-  </ul> </div>  </div>  </div>  </div>  </div>`
-
+  </ul> </div>  </div>  </div>  </div>  </div>`;
 
 const openBtn = document.querySelectorAll('[data-modal-target]');
 const popUpSection = document.querySelector('.pop-up-section');
 let closeBtn;
 
-openBtn.forEach(button => {
+openBtn.forEach((button) => {
   button.addEventListener('click', () => {
     popUpSection.innerHTML = '';
     popUpSection.appendChild(div1Popup);
