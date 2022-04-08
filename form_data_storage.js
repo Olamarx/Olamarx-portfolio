12cef9c95cae330f5393941a2ccd9033583c7b6e
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-08 08:32:33 +0100

    Local storage function where the data would be stored and retained even after reload

---
+++ form_data_storage.js
@@ -0,0 +1,35 @@
+const formPickDetail = document.querySelector('#Form-contact')
+// console.log(formPickDetail)
+
+let formData = {
+    name: '',
+    email: '',
+    text: '',
+}
+// console.log(formData)
+
+function formInputData(inputType) {
+    formPickDetail.elements[inputType].addEventListener('input', () => {
+        formData[inputType] = formPickDetail.elements[inputType].value;
+        localStorage.setItem('form-data', JSON.stringify(formData));
+    });
+}
+
+function setFormDataInLocaleStorage(inputType) {
+    formPickDetail.elements[inputType].value = JSON.parse(localStorage['form-data'])[inputType];
+};
+
+window.addEventListener('load', () => {
+    if (localStorage.getItem('form-data') === null ) {
+        localStorage.setItem('form-data', JSON.stringify(formData));
+    } else {
+        formData = JSON.parse(localStorage.getItem('form-data'))
+    }
+
+    formInputData('name');
+    formInputData('email');
+    formInputData('text');
+    setFormDataInLocaleStorage('name');
+    setFormDataInLocaleStorage('email');
+    setFormDataInLocaleStorage('text');
+});
